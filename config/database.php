ca8d8658bc97eab439858ad247c9a66f1cfb5dbe
TrueLecter <andrew.panassiouk@gmail.com>
2014-11-06 14:42:42 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,10 +8,3 @@
         $q = 'create table if not exists '.$table.' (`name` text, `money` double, `type` text, `dateTime` text, `comment` text, `timeStamp` int, `id` int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))';
         mysqli_query($link, $q);
 ?>		
-=======
-	$host = 'mysql.hostinger.ru';
-	$user = 'u885629350_roooo';
-	$password = '123456';
-	$db = 'u885629350_usic';
-	$link = mysqli_connect($host, $user, $password, $db);
-?>
